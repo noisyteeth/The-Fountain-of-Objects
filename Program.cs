eb1de00d3728e33923877378064fbeaafe5f2ed8
noisyteeth <noisyteeth@protonmail.com>
2023-07-26 22:17:14 +0700

    added ShowRoomsStatus() to Game
    ShowRoomsStatus() displays the player's location and the coordinates of all the rooms.

--- Program.cs
+++ Program.cs
@@ -167,12 +167,6 @@ public class Game
         _pitIsNear = false;
     }
 
-    public void Prompts()
-    {
-        if (_pitIsNear)
-            Console.WriteLine("You feel a draft. There is a pit in a nearby room.");
-    }
-
     public void Prompts(Map map)
     {
         if (map.PlayerRoom == RoomTypes.Entrance)
@@ -185,135 +179,20 @@ public class Game
             else
                 Console.WriteLine("You hear water dripping in this room. The Fountain of Objects is here!");
         }
+
+        if (_pitIsNear)
+            Console.WriteLine("You feel a draft. There is a pit in a nearby room.");
     }
-}
 
-public class Cavern
-{
-    // All indexes in _grid are false by default. After making a new
-    // instance of Cavern, call UpdatePlayerPos to set the starting point.
-    private bool[,] _grid;
-    private bool _fountainRoom = false;
-    private bool _fountainEnabled = false;
-    private bool _win = false;
-    private bool _entrance = false;
-
-    private readonly int _gridSize;
-
-    private Coord FountainCoord
+    public void ShowRoomsStatus(Map map)
     {
-        get
+        for (int x = 0; x < map.GridSize; x++)
         {
-            if (_gridSize == 6) return new Coord(3, 0);
-            else if (_gridSize == 8) return new Coord(4, 0);
-            return new Coord(2, 0);
-        }
-    }
-
-    public bool[,] Grid => _grid;
-    public bool FountainRoom => _fountainRoom;
-    public bool FountainEnabled => _fountainEnabled;
-    public bool Entrance => _entrance;
-    public bool Win => _win;
-    public int GridSize => _gridSize;
-
-    public Cavern()
-    {
-        _gridSize = 4;
-        _grid = new bool[4, 4];
-    }
-
-    public Cavern(int gridSize)
-    {
-        _gridSize = gridSize;
-        _grid = new bool[gridSize, gridSize];
-    }
-    
-    public void UpdatePlayerPos(Player player)
-    {
-        int xPos = player.PlayerCoord.X;
-        int yPos = player.PlayerCoord.Y;
-
-        for (int x = 0; x < _grid.GetLength(0); x++)
-        {
-            for (int y = 0; y < _grid.GetLength(1); y++)
+            for (int y = 0; y < map.GridSize; y++)
             {
-                if (y == yPos && x == xPos)
-                    _grid[x, y] = true;
-                else _grid[x, y] = false;
-
-                if (xPos == FountainCoord.X && yPos == FountainCoord.Y) _fountainRoom = true;
-                else _fountainRoom = false;
-
-                if (xPos == 0 && yPos == 0)
-                    _entrance = true;
-                else _entrance = false;
+                Console.WriteLine(String.Format("{0,-16} ({1}, {2}) {3}", map.Rooms[x, y], x, y, map.PlayerLoc[x, y]));
             }
         }
-    }
-
-    /// <summary>
-    /// Shows the current location of the player within a grid representation of the world.
-    /// Also shows conditions that have or haven't been fulfulled.
-    /// Only for testing purposes.
-    /// </summary>
-    public void ShowRoomsStatus()
-    {
-        for (int y = 0; y < Grid.GetLength(0); y++)
-        {
-            Console.WriteLine();
-
-            for (int x = 0; x < Grid.GetLength(1); x++)
-            {
-                if (Grid[x, y] == true) Console.ForegroundColor = ConsoleColor.Green;
-                Console.Write(String.Format("({0},{1})", x, y));
-                Console.ResetColor();
-                Console.Write(" | ");
-            }
-        }
-
-        Console.WriteLine();
-        Console.WriteLine("---------------------------------------------------------------");
-        Console.WriteLine($"FountainRoom = {FountainRoom}");
-        Console.WriteLine($"FountainEnabled = {FountainEnabled}");
-        Console.WriteLine($"Entrance = {Entrance}");
-        Console.WriteLine();
-    }
-
-    /// <summary>
-    /// Check if a room is a "special room", i.e. the fountain room or the entrance.
-    /// </summary>
-    public void CheckSpecialRoom()
-    {
-        if (FountainRoom == true)
-        {
-            if (FountainEnabled == false)
-                Console.WriteLine("You hear water dripping in this room. The Fountain of Objects is here!");
-
-            else 
-                Console.WriteLine("You hear the rushing waters from the Fountain of Objects. It has been reactivated!");
-        }
-
-        if (Entrance == true)
-        {
-            if (FountainEnabled == true)
-            {
-                _win = true;
-                Console.WriteLine("The Fountain of Objects has been reactivated, and you have escaped with your life!");
-            }
-            
-            else Console.WriteLine("You see light coming from the cavern entrance.");
-        }
-    }
-
-    /// <summary>
-    /// Enables the fountain if the player is in the fountain room and the fountain is disabled.
-    /// </summary>
-    /// <param name="command"></param>
-    public void EnableFountain(string command)
-    {
-        if (command == "enable fountain" && FountainEnabled == false && FountainRoom == true)
-            _fountainEnabled = true;
     }
 }
 
