bff41737fcbfe6511c18a76116e77c2a1a774d92
noisyteeth <noisyteeth@protonmail.com>
2023-07-28 10:18:22 +0700

    moved classes to their own files
    Each class now has its own file. All classes fall under the `Fountain` namespace.

---
+++ Coord.cs
@@ -0,0 +1,69 @@
+﻿
+namespace Fountain
+{
+    public class Coord
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Coord(int row, int column)
+        {
+            X = row;
+            Y = column;
+        }
+
+        public Coord()
+        {
+            X = 0;
+            Y = 0;
+        }
+
+        public static bool IsAdjacent(Coord first, Coord second)
+        {
+            int rowDifference = Math.Abs(first.X - second.X);
+            int columnDifference = Math.Abs(first.Y - second.Y);
+
+            bool rowAdjacent = false;
+            bool columnAdjacent = false;
+
+            if (rowDifference <= 1 && rowDifference >= -1)
+                rowAdjacent = true;
+            if (columnDifference <= 1 && columnDifference >= -1)
+                columnAdjacent = true;
+
+            if (rowAdjacent && columnAdjacent) return true;
+
+            return false;
+        }
+
+        public static bool IsAdjacent(Coord first, int x, int y)
+        {
+            int rowDifference = Math.Abs(first.X - x);
+            int columnDifference = Math.Abs(first.Y - y);
+
+            bool rowAdjacent = false;
+            bool columnAdjacent = false;
+
+            if (rowDifference <= 1 && rowDifference >= -1)
+                rowAdjacent = true;
+            if (columnDifference <= 1 && columnDifference >= -1)
+                columnAdjacent = true;
+
+            if (rowAdjacent && columnAdjacent) return true;
+
+            return false;
+        }
+
+        public static bool SameCoord(Coord first, Coord second)
+        {
+            if (first.X == second.X && first.Y == second.Y) return true;
+            return false;
+        }
+
+        public static bool SameCoord(Coord first, int x, int y)
+        {
+            if (first.X == x && first.Y == y) return true;
+            return false;
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,52 @@
+﻿
+namespace Fountain
+{
+    public class Player
+    {
+        private bool _dead;
+        private Coord _playerCoord;
+
+        public bool Dead => _dead;
+        public Coord PlayerCoord => _playerCoord;
+        public string PlayerCoordString => $"({PlayerCoord.X}, {PlayerCoord.Y})";
+
+        public Player()
+        {
+            _dead = false;
+            _playerCoord = new(0, 0);
+        }
+
+        public void MovePlayer(Direction direction, Map map)
+        {
+            switch (direction)
+            {
+                case Direction.North:
+                    if (PlayerCoord.Y - 1 < 0) return;
+                    PlayerCoord.Y--;
+                    break;
+                case Direction.South:
+                    if (PlayerCoord.Y + 1 > map.GridSize - 1) return;
+                    PlayerCoord.Y++;
+                    break;
+                case Direction.East:
+                    if (PlayerCoord.X + 1 > map.GridSize - 1) return;
+                    PlayerCoord.X++;
+                    break;
+                case Direction.West:
+                    if (PlayerCoord.X - 1 < 0) return;
+                    PlayerCoord.X--;
+                    break;
+            }
+        }
+
+        public void ShowPlayerCoord()
+        {
+            Console.WriteLine(PlayerCoordString);
+        }
+
+        public void Death(Map map)
+        {
+            if (map.PlayerRoom == RoomTypes.Pit) _dead = true;
+        }
+    }
+}
