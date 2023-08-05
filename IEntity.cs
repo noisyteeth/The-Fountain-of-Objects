deca3d6bd89121e94c6f6c71df9d35e0b26918ae
noisyteeth <noisyteeth@protonmail.com>
2023-08-05 23:13:50 +0700

    created NewCoord record
    When referring to coordinates, this will be used in place of Coord

--- IEntity.cs
+++ IEntity.cs
@@ -3,6 +3,6 @@ namespace Fountain;
 
 public interface IEntity
 {
-    public Coord Position { get; set; }
+    public NewCoord Position { get; set; }
     public bool Dead { get; set; }
 }
---
+++ NewCoord.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Fountain;
+
+public record NewCoord(int X, int Y)
+{
+    public static NewCoord operator +(NewCoord a, NewCoord b)
+        => new NewCoord(a.X + b.X, a.Y + b.Y);
+
+    public static NewCoord operator +(NewCoord a, Direction dir)
+    {
+        return dir switch
+        {
+            Direction.North => new(a.X, a.Y - 1),
+            Direction.South => new(a.X, a.Y + 1),
+            Direction.East => new(a.X + 1, a.Y),
+            Direction.West => new(a.X - 1, a.Y)
+        };
+    }
+
+    public static NewCoord operator +(Direction dir, NewCoord a)
+        => a + dir;
+}
