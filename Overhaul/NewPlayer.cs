e38747970c1fc9540c5683f1e4dbecdc4250eae9
noisyteeth <noisyteeth@protonmail.com>
2023-08-05 23:57:14 +0700

    added constructors for NewPlayer

--- Overhaul/NewPlayer.cs
+++ Overhaul/NewPlayer.cs
@@ -8,6 +8,16 @@ public class NewPlayer : IEntity
     public NewCoord Position { get; set; }
     public bool Dead { get; set; }
 
+    public NewPlayer(NewCoord position)
+    {
+        Position = position;
+    }
+
+    public NewPlayer()
+    {
+        Position = new(0, 0);
+    }
+
     public void MoveEntity(NewCoord offset)
     {
         Position += offset;
