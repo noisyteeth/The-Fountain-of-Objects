e1beb262b779d33d8f58a221521e40b9eaa075b8
noisyteeth <noisyteeth@protonmail.com>
2023-08-05 23:46:22 +0700

    created NewPlayer class
    Will take Player's place

---
+++ Overhaul/NewPlayer.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Fountain;
+
+public class NewPlayer : IEntity
+{
+    public event Action? Death;
+
+    public NewCoord Position { get; set; }
+    public bool Dead { get; set; }
+
+    public void MoveEntity(NewCoord offset)
+    {
+        Position += offset;
+    }
+}
