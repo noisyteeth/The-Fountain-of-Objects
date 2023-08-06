e4f063285160555615c16da580748cabbcaf533f
noisyteeth <noisyteeth@protonmail.com>
2023-08-06 09:12:58 +0700

    created new Empty type that extends from Entity

--- Overhaul/Entity.cs
+++ Overhaul/Entity.cs
@@ -3,11 +3,32 @@ namespace Fountain;
 
 public abstract class Entity
 {
-    public event Action? Death;
+    //public event Action? Death;
 
     private NewCoord _position;
     private bool _dead;
 
     public abstract void MoveEntity(NewCoord offset);
     public abstract void MoveEntity(Direction dir);
+}
+
+public class Empty : Entity
+{
+    private NewCoord _position;
+    private bool _dead;
+
+    public Empty(NewCoord position)
+    {
+        _position = position;
+    }
+
+    public override void MoveEntity(NewCoord offset)
+    {
+        _position += offset;
+    }
+
+    public override void MoveEntity(Direction dir)
+    {
+        _position += dir;
+    }
 }
--- Overhaul/World.cs
+++ Overhaul/World.cs
@@ -8,6 +8,6 @@ public class World
 
 public class Room
 {
-    public NewPlayer Player { get; set; }
+    public Entity Entity { get; set; }
     public RoomTypes RoomType { get; set; }
 }
