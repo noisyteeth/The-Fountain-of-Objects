9b974efc6ec8e1663a20e5edfe3864e8b1500cce
noisyteeth <noisyteeth@protonmail.com>
2024-02-22 22:40:16 +0700

    Create Tests.cs

---
+++ Overhaul/Tests.cs
@@ -0,0 +1,54 @@
+﻿
+using Fountain;
+
+/*
+NewPlayer player = new();
+
+World world = new(4, player);
+
+player.Teleport(2, -5);
+player.Teleport(1, 0);
+player.MoveByOffset(5, 0);
+
+Console.WriteLine(player.Position);
+
+foreach (var ent in world.EntityMap)
+{
+    Console.WriteLine(ent);
+}
+
+Foo foo = new(4);
+Bar bar = new(foo);
+
+foo.CallEvent();
+
+public class Foo
+{
+    public event Action<Foo> DoSomething;
+
+    public int Value { get; } 
+
+    public Foo(int value)
+    {
+        Value = value;
+    }
+
+    public void CallEvent()
+    {
+        DoSomething(this);
+    }
+}
+
+public class Bar
+{
+    public Bar(Foo foo)
+    {
+        foo.DoSomething += PrintValue;
+    }
+
+    public void PrintValue(Foo foo)
+    {
+        Console.WriteLine($"The value is {foo.Value}");
+    }
+}
+*/
